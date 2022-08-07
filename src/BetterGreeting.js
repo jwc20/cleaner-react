b156c44ddb1974aea0513f41b0c13d9b94846422
jwc20 <choij2015@gmail.com>
2022-08-07 13:09:21 -0700

    added another editable input inside a function to demonstrate you can use hooks more than once

--- src/BetterGreeting.js
+++ src/BetterGreeting.js
@@ -3,9 +3,14 @@ import { useState } from "react";
 
 export default function BetterGreeting(props) {
   const [name, setName] = useState("Mary");
+  const [surname, setSurname] = useState("Poppins");
 
   function handleNameChange(e) {
     setName(e.target.value);
+  }
+
+  function handleSurnameChange(e) {
+    setSurname(e.target.value);
   }
 
   return (
@@ -13,6 +18,10 @@ export default function BetterGreeting(props) {
       <h1>This is a better greeting</h1>
       <Row lable="Name">
         <input value={name} onChange={handleNameChange} />
+      </Row>
+
+      <Row lable="Surname">
+        <input value={surname} onChange={handleSurnameChange} />
       </Row>
     </section>
   );
